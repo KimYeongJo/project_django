f83b3705721b4a2552c0b01a9c79a12f162f2af1
KimYeongJo <132258199+KimYeongJo@users.noreply.github.com>
2023-07-18 12:20:16 +0900

    user model 구현 완료

--- blogapp/user/views.py
+++ blogapp/user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+class Register(View):
+    pass
+
+
+class Login(View):
+    pass
+
+
+class Logout(View):
+    pass
